c1b8e030788c3efa7d9c4f496c60a354b3906f93
Abdelrhman Eliwa <abdelrhmankamaleliwa@gmail.com>
2021-08-13 09:18:58 +0200

    Adding Categories enum model

--- NewsApp/Scenes/Onboarding/Presenter/OnboardingPresenter.swift
+++ NewsApp/Scenes/Onboarding/Presenter/OnboardingPresenter.swift
@@ -7,39 +7,6 @@
 
 import Foundation
 
-enum Categories: Int {
-    case business = 1
-    case entertainment = 2
-    case general = 3
-    case health = 4
-    case science = 5
-    case sports = 6
-    case technology = 7
-    
-    static func getCategoriesTitles(_ categories: [Categories]) -> [String] {
-        var titles: [String] = []
-        categories.forEach { category in
-            switch category {
-            case .business:
-                titles.append("business")
-            case .entertainment:
-                titles.append("entertainment")
-            case .general:
-                titles.append("general")
-            case .health:
-                titles.append("health")
-            case .science:
-                titles.append("science")
-            case .sports:
-                titles.append("sports")
-            case .technology:
-                titles.append("technology")
-            }
-        }
-        return titles
-    }
-}
-
 class OnboardingPresenter: OnBoardingPresenterProtocol {
     
     // MARK: - Properties
